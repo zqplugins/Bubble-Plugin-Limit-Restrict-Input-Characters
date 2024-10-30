f7404fd18ca5b250c0e58d40a1ba9c4f3feea001
Andrii Pichkorskiy <plugins@zeroqode.com>
2024-10-30 15:03:49 +0200

    obfuscate

---
+++ elementsObfuscated/AAC-850m6/initialize.js
@@ -0,0 +1,4 @@
+function(instance, context){
+	function initialize(_0x183d2e,_0x5b231f){}
+	return  initialize(instance, context);
+}
---
+++ elementsObfuscated/AAC-850m6/reset.js
@@ -0,0 +1,4 @@
+function(instance, context){
+	function reset(_0x4f021e,_0x588a5f){}
+	return  reset(instance, context);
+}
